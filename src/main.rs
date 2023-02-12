39132b03ab70fc27ecb80ef1b88c4a0643aaf3fe
Sammy Nouri <sammy_nouri@yahoo.com>
2023-02-12 15:29:00 +0300

    small fixes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use nannou::prelude::*;
 
 const SMILEY_RADIUS: f32 = 200.0;
-const SMILEY_EYE_RADIUS: f32 = 20.0;
-const STROKE_WEIGHT: f32 = 10.0;
+const SMILEY_EYE_RADIUS: f32 = 25.0;
+const SMILE_STROKE: f32 = 15.0;
+const CIRCLE_STROKE: f32 = 10.0;
 const EYE_INITIAL_POSITION_X: f32 = 100.0;
 const EYE_INITIAL_POSITION_Y: f32 = 80.0;
+const EYE_ANGLE_OFFSET: f32 = 0.8;
 
 pub struct Smiley {
     rotation: f32,
@@ -17,7 +19,7 @@ impl Smiley {
         let points: Vec<Point2> = (-1000..1000)
             .map(|i| {
                 let x = i as f32 * 0.1;
-                let y = 0.005 * (x * x) - 100.0;
+                let y = 0.005 * (x * x) - 110.0;
                 pt2(x, y)
             })
             .collect();
@@ -30,6 +32,12 @@ impl Smiley {
                 (EYE_INITIAL_POSITION_X, EYE_INITIAL_POSITION_Y),
             ],
         }
+    }
+}
+
+impl Default for Smiley {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -53,11 +61,11 @@ fn model(app: &App) -> Smiley {
 }
 
 fn update(_app: &App, model: &mut Smiley, _update: Update) {
-    model.rotation += 0.01;
-    model.eyes[0].0 = EYE_INITIAL_POSITION_X * model.rotation.cos();
-    model.eyes[0].1 = EYE_INITIAL_POSITION_Y * model.rotation.sin();
-    model.eyes[1].0 = -EYE_INITIAL_POSITION_X * model.rotation.sin();
-    model.eyes[1].1 = EYE_INITIAL_POSITION_Y * model.rotation.cos();
+    model.rotation += 0.005;
+    model.eyes[0].0 = EYE_INITIAL_POSITION_X * (model.rotation + EYE_ANGLE_OFFSET).cos();
+    model.eyes[0].1 = EYE_INITIAL_POSITION_Y * (model.rotation + EYE_ANGLE_OFFSET).sin();
+    model.eyes[1].0 = -EYE_INITIAL_POSITION_X * (model.rotation + EYE_ANGLE_OFFSET).sin();
+    model.eyes[1].1 = EYE_INITIAL_POSITION_Y * (model.rotation + EYE_ANGLE_OFFSET).cos();
 }
 
 fn view(app: &App, model: &Smiley, frame: Frame) {
@@ -70,7 +78,7 @@ fn view(app: &App, model: &Smiley, frame: Frame) {
         .color(YELLOW)
         .radius(SMILEY_RADIUS)
         .stroke(BLACK)
-        .stroke_weight(STROKE_WEIGHT);
+        .stroke_weight(CIRCLE_STROKE);
 
     draw.ellipse()
         .color(BLACK)
@@ -91,7 +99,7 @@ fn view(app: &App, model: &Smiley, frame: Frame) {
     });
 
     draw.polyline()
-        .weight(10.0)
+        .weight(SMILE_STROKE)
         .points_colored(points)
         .rotate(model.rotation);
 
@@ -99,4 +107,19 @@ fn view(app: &App, model: &Smiley, frame: Frame) {
 
     // Write to the window frame.
     draw.to_frame(app, &frame).unwrap();
+
+    // let file_path = captured_frame_path(app, &frame);
+    // app.main_window().capture_frame(file_path);
 }
+
+// fn captured_frame_path(app: &App, frame: &Frame) -> std::path::PathBuf {
+//     // Create a path that we want to save this frame to.
+//     app.project_path()
+//         .expect("failed to locate `project_path`")
+//         // Capture all frames to a directory called `/<path_to_nannou>/nannou/simple_capture`.
+//         .join(app.exe_name().unwrap())
+//         // Name each file after the number of the frame.
+//         .join(format!("{:03}", frame.nth()))
+//         // The extension will be PNG. We also support tiff, bmp, gif, jpeg, webp and some others.
+//         .with_extension("png")
+// }
